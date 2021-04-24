1b9f50b3539bef69cdc0638b0fdd959ee1588002
Istvan Ugrai <iugrai@yahoo.com>
2021-04-24 10:46:49 +0200

    buidings órai része kész

--- buildings/app.js
+++ buildings/app.js
@@ -1,15 +1,28 @@
 console.log('hello');
-
+// amikor a user rákattint a menu gombra:
 document.querySelector('.menu-button').addEventListener('click', function () {
   console.log('clicked!');
   document.querySelector('body').classList.add('js-menu-open');
-  document.querySelector('.close-menu').classList.add('js-hidden');
-
+  document.querySelector('.close-menu').classList.remove('js-hidden');
+  document.querySelector('header .main-menu').classList.add('js-show');
 });
 
+// amikor a user rákattint a close menu gombra
 document.querySelector('.close-menu').addEventListener('click', function () {
   console.log('clicked!');
   document.querySelector('body').classList.remove('js-menu-open');
   document.querySelector('.close-menu').classList.add('js-hidden');
-
+  document.querySelector('header .main-menu').classList.remove('js-show');
+});
+// AMIKOR A USER ÁTMÉRETEZI A BÖNGÉSZŐ ABLAKOT
+window.addEventListener('resize', function () {
+  // megvizsgáljuk hogy nyitva van-e a menü:
+  if (document.querySelector('body').classList.contains('js-menu-open')) {
+    // nyitva van a menu...
+    if (window.innerWidth >= 650) {
+      // ha a böngésző szélessége 650 fölé megy és nyitva van a menü
+      // amikor a javascript automatikusan rákattint helyettünk a close gombra
+      document.querySelector('.close-menu').click();
+    }
+  }
 });
